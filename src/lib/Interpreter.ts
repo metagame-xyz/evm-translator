0e66139b9f3205d9d5349d6db56042ce6158a4af
Miguel Piedrafita <github@miguelpiedrafita.com>
2022-02-28 13:17:11 +0100

    wip

---
+++ src/lib/Interpreter.ts
@@ -0,0 +1,61 @@
+import { ActivityEntry } from './Activity'
+import Inspector, { Config, InspectorResult } from './Inspector'
+import ContractDeploy from './inspectors/ContractDeploy'
+import ContractInteraction from './inspectors/ContractInteraction'
+import ENSRenewal from './inspectors/ENSRenewal'
+import ETHTransfer from './inspectors/ETHTransfer'
+import ExternalInteraction from './inspectors/ExternalInteraction'
+import MintReceived from './inspectors/MintReceived'
+import NFTMint from './inspectors/NFTMint'
+import TokensSent from './inspectors/TokensSent'
+import OpenSeaBuy from './inspectors/OpenSeaBuy'
+import OpenSeaCancel from './inspectors/OpenSeaCancel'
+import TokensReceived from './inspectors/TokensReceived'
+import UniswapV2Swap from './inspectors/UniswapV2Swap'
+import LooksRareSale from './inspectors/LooksRareSale'
+import TokenApproval from './inspectors/TokenApproval'
+import SpamTransaction from './inspectors/SpamTransaction'
+
+const INSPECTORS = [
+	/* Specific Providers */
+	new OpenSeaBuy(),
+	new OpenSeaCancel(),
+	new ENSRenewal(),
+	new UniswapV2Swap(),
+	new LooksRareSale(),
+
+	/* General Sent */
+	new TokensSent(),
+	new NFTMint(),
+	new TokenApproval(),
+
+	/* General Received */
+	new TokensReceived(),
+	new MintReceived(),
+
+	/* Spam Filter */
+	new SpamTransaction(),
+
+	/* Fallbacks */
+	new ContractInteraction(),
+	new ExternalInteraction(),
+
+	/* Other */
+	new ETHTransfer(),
+	new ContractDeploy(),
+]
+
+class Interpreter {
+	#inspectors: Array<Inspector> = INSPECTORS
+
+	public augment(entry: ActivityEntry, config: Config): InspectorResult {
+		return this.#inspectors
+			.find(inspector => {
+				console.log(`checking ${inspector.name}`)
+				return inspector.check(entry, config)
+			})
+			.resolve(entry, config)
+	}
+}
+
+export default new Interpreter()
