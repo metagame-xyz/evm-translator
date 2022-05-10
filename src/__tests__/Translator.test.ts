eb3243271aa2f11455a0ea3d5c6eab159ad753d5
Brenner Spear <blspear@gmail.com>
2022-05-10 15:20:44 -0500

    mock node-fetch in testing

--- src/__tests__/Translator.test.ts
+++ src/__tests__/Translator.test.ts
@@ -1,6 +1,8 @@
 import Translator, { createEthersAPIKeyObj, TranslatorConfig } from 'Translator'
 import { chains } from 'utils'
 
+jest.mock('node-fetch', () => jest.fn())
+
 test('Translator', () => {
     const translatorConfig: TranslatorConfig = {
         chain: chains.ethereum,
