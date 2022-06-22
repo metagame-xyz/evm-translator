3b1d8a07599bb27c5f220f327d2d29778ce5b58a
leojacoby <leojacoby@gmail.com>
2022-06-22 18:02:47 -0400

    Compound interpretation!

--- src/utils/abi-decoder.ts
+++ src/utils/abi-decoder.ts
@@ -82,11 +82,11 @@ export default class ABIDecoder {
         console.log('abiSig', abiSig)
         return abiSig
             ? {
-                  name: abiSig,
-                  type: 'event',
-                  inputs: [],
-                  anonymous: false,
-              }
+                name: abiSig,
+                type: 'event',
+                inputs: [],
+                anonymous: false,
+            }
             : undefined
     }
     async getABIFunctionFromExternalSource(hexSignature: string): Promise<ABI_Function | undefined> {
@@ -94,12 +94,12 @@ export default class ABIDecoder {
 
         return contractMethod
             ? {
-                  name: contractMethod,
-                  type: 'function',
-                  inputs: [],
-                  outputs: [],
-                  stateMutability: undefined,
-              }
+                name: contractMethod,
+                type: 'function',
+                inputs: [],
+                outputs: [],
+                stateMutability: undefined,
+            }
             : undefined
     }
 
@@ -170,13 +170,13 @@ export default class ABIDecoder {
             logs
                 .filter((log) => log.topics.length > 0)
                 .map(async (logItem) => {
+
                     const eventID = logItem.topics[0]
                     const address = AddressZ.parse(logItem.address)
 
                     // first check if it the contract's abi has it
                     let abiItem: ABI_Event | null = this.eventSigs[address] ? this.eventSigs[address][eventID] : null
 
-                    // if it's there, add it as an option
                     let abiItemOptions: ABI_Event[] = []
 
                     // if not, and it's the ambiguous Transfer event, check the contract type
@@ -219,11 +219,12 @@ export default class ABIDecoder {
                         try {
                             decodedData = getDecodedData(logItem, abiItem)
                         } catch (e) {
-                            //try again
+                            abiItemOptions = (await this.db.getEventABIsForHexSignature(eventID)) || []
                         }
-                    } else if (abiItemOptions.length > 0) {
+                    }
+                    if (abiItemOptions.length > 0) {
                         // try all of the options, it'll throw an error if it doesn't match, catch it, try the next one
-                        while (!decodedData && abiItemOptions.length > 0) {
+                        while ((!decodedData || !Object.keys(decodedData).length) && abiItemOptions.length > 0) {
                             abiItem = abiItemOptions.shift() as ABI_Event
                             try {
                                 decodedData = getDecodedData(logItem, abiItem)
@@ -233,7 +234,7 @@ export default class ABIDecoder {
                         }
                     }
 
-                    if (abiItem && decodedData) {
+                    if (abiItem && decodedData && Object.keys(decodedData).length) {
                         type DecodedParam = {
                             name: string
                             value: string | BigNumber
@@ -312,13 +313,13 @@ export default class ABIDecoder {
             if (abi.name) {
                 const signature = hash(
                     abi.name +
-                        '(' +
-                        abi.inputs
-                            .map(function (input) {
-                                return input.type
-                            })
-                            .join(',') +
-                        ')',
+                    '(' +
+                    abi.inputs
+                        .map(function (input) {
+                            return input.type
+                        })
+                        .join(',') +
+                    ')',
                 ) as string
                 if (abi.type === 'event') {
                     if (this.methodSigs[signature]) {
