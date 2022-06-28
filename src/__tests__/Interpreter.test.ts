5f160526b1f468c9e0b89a848a7e6c27f2fd954c
leojacoby <leojacoby@gmail.com>
2022-06-28 13:36:43 -0400

    handle strict _from field in Aave

--- src/__tests__/Interpreter.test.ts
+++ src/__tests__/Interpreter.test.ts
@@ -1,31 +1,34 @@
+import testTxHashes, { flattenTxHashes } from '../__tests__/testTxHashes.js'
 import Interpreter from '../core/Interpreter'
 import { chains } from '../index'
-import testTxHashes, { flattenTxHashes } from '../__tests__/testTxHashes.js'
-import { MongooseDatabaseInterface } from 'utils/mongoose'
 import { DecodedTx } from '../interfaces/decoded'
 import dotenv from 'dotenv'
+
+import { MongooseDatabaseInterface } from 'utils/mongoose'
 
 dotenv.config()
 
 jest.mock('node-fetch', () => jest.fn())
 
-jest.useRealTimers();
+jest.useRealTimers()
 
 test('Interpreter', async () => {
-  jest.setTimeout(200 * 1000);
-  const db = new MongooseDatabaseInterface(process.env.EVM_TRANSLATOR_CONNECTION_STRING || '')
-  await db.connect()
-  const interpreter = new Interpreter(chains.ethereum)
-  const txMap = await db.getManyDecodedTxMap(flattenTxHashes())
-  const filteredDecodedTxes = Object.entries(txMap).filter(([txHash, decodedTx]) => {
-    if (!decodedTx) {
-      console.log('Issue with decoder for tx:', txHash)
-      return false
-    }
-    return true
-  }).map(([, decodedTx]) => (decodedTx))
+    jest.setTimeout(200 * 1000)
+    const db = new MongooseDatabaseInterface(process.env.EVM_TRANSLATOR_CONNECTION_STRING || '')
+    await db.connect()
+    const interpreter = new Interpreter(chains.ethereum)
+    const txMap = await db.getManyDecodedTxMap(flattenTxHashes())
+    const filteredDecodedTxes = Object.entries(txMap)
+        .filter(([txHash, decodedTx]) => {
+            if (!decodedTx) {
+                console.log('Issue with decoder for tx:', txHash)
+                return false
+            }
+            return true
+        })
+        .map(([, decodedTx]) => decodedTx)
 
-  for (const decodedTx of filteredDecodedTxes) {
-    expect(interpreter.interpretSingleTx(decodedTx!)).toMatchSnapshot()
-  }
+    for (const decodedTx of filteredDecodedTxes) {
+        expect(interpreter.interpretSingleTx(decodedTx!)).toMatchSnapshot()
+    }
 }, 200000)
--- src/scripts/seed-db-test-decoded-txs.ts
+++ src/scripts/seed-db-test-decoded-txs.ts
@@ -1,16 +1,17 @@
-import { MongooseDatabaseInterface } from 'utils/mongoose'
 import { flattenTxHashes } from '../__tests__/testTxHashes'
+import getTranslator from '../utils/translator'
 import dotenv from 'dotenv'
+
+import { MongooseDatabaseInterface } from 'utils/mongoose'
+
 dotenv.config()
 
-import getTranslator from '../utils/translator';
-
 async function run() {
-  const db = new MongooseDatabaseInterface('mongodb://localhost:27017/evm-translator')
-  await db.connect()
-  const translator = await getTranslator()
-  await translator.allDataFromTxHashArr(flattenTxHashes());
-  process.exit(0)
+    const db = new MongooseDatabaseInterface('mongodb://localhost:27017/evm-translator')
+    await db.connect()
+    const translator = await getTranslator()
+    await translator.allDataFromTxHashArr(flattenTxHashes())
+    process.exit(0)
 }
 
 run()
