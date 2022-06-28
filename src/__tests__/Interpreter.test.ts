fe878c3f10c2667c46aa181dbabddc7612a6cb89
leojacoby <leojacoby@gmail.com>
2022-06-28 14:01:33 -0400

    Ok for real i'm done this time

--- src/__tests__/Interpreter.test.ts
+++ src/__tests__/Interpreter.test.ts
@@ -5,8 +5,6 @@ import { DecodedTx } from '../interfaces/decoded'
 import dotenv from 'dotenv'
 import { MongooseDatabaseInterface } from 'utils/mongoose'
 
-import { MongooseDatabaseInterface } from 'utils/mongoose'
-
 dotenv.config()
 
 jest.mock('node-fetch', () => jest.fn())
@@ -14,22 +12,23 @@ jest.mock('node-fetch', () => jest.fn())
 jest.useRealTimers()
 
 test('Interpreter', async () => {
-    jest.setTimeout(200 * 1000)
-    const db = new MongooseDatabaseInterface(process.env.EVM_TRANSLATOR_CONNECTION_STRING || '')
-    await db.connect()
-    const interpreter = new Interpreter(chains.ethereum)
-    const txMap = await db.getManyDecodedTxMap(flattenTxHashes())
-    const filteredDecodedTxes = Object.entries(txMap)
-        .filter(([txHash, decodedTx]) => {
-            if (!decodedTx) {
-                console.log('Issue with decoder for tx:', txHash)
-                return false
-            }
-            return true
-        })
-        .map(([, decodedTx]) => decodedTx)
+  jest.setTimeout(200 * 1000)
+  const db = new MongooseDatabaseInterface(process.env.EVM_TRANSLATOR_CONNECTION_STRING || '')
+  await db.connect()
+  const interpreter = new Interpreter(chains.ethereum)
+  const txMap = await db.getManyDecodedTxMap(flattenTxHashes())
+  const filteredDecodedTxes =
+    Object.entries(txMap)
+      .filter(([txHash, decodedTx]) => {
+        if (!decodedTx) {
+          console.log('Issue with decoder for tx:', txHash)
+          return false
+        }
+        return true
+      })
+      .map(([, decodedTx]) => decodedTx)
 
-    for (const decodedTx of filteredDecodedTxes) {
-        expect(interpreter.interpretSingleTx(decodedTx!)).toMatchSnapshot()
-    }
+  for (const decodedTx of filteredDecodedTxes) {
+    expect(interpreter.interpretSingleTx(decodedTx!)).toMatchSnapshot()
+  }
 }, 200000)
