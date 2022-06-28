02b42641dd444b04aaf7f41a3b0dd70978bd2fed
leojacoby <leojacoby@gmail.com>
2022-06-28 15:44:26 -0400

    fix snapshot resolver

--- src/__tests__/snapshotResolver.js
+++ src/__tests__/snapshotResolver.js
@@ -1,5 +1,5 @@
 module.exports = {
   resolveSnapshotPath: (testPath, snapshotExtension) => 'src/__tests__/__snapshots__' + testPath.slice(testPath.lastIndexOf('/')) + snapshotExtension,
-  resolveTestPath: (snapshotFilePath, snapshotExtension) => '/lib/__tests__' + snapshotFilePath.slice(snapshotFilePath.lastIndexOf('/'), -snapshotExtension.length),
-  testPathForConsistencyCheck: '/lib/__tests__/some.test.js',
+  resolveTestPath: (snapshotFilePath, snapshotExtension) => 'lib/__tests__' + snapshotFilePath.slice(snapshotFilePath.lastIndexOf('/'), -snapshotExtension.length),
+  testPathForConsistencyCheck: 'lib/__tests__/some.test.js',
 };
