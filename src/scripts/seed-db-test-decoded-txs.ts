34456cf87960b9648cbc60ed4cbd4c62310dcc04
leojacoby <leojacoby@gmail.com>
2022-06-28 15:28:28 -0400

    setup env for seed-db-test-txs

--- src/scripts/seed-db-test-decoded-txs.ts
+++ src/scripts/seed-db-test-decoded-txs.ts
@@ -1,17 +1,11 @@
 import { flattenTxHashes } from '../__tests__/testTxHashes'
+import './setupEnv'
 import getTranslator from '../utils/translator'
-import dotenv from 'dotenv'
-
-import { MongooseDatabaseInterface } from 'utils/mongoose'
-
-dotenv.config()
 
 async function run() {
-    const db = new MongooseDatabaseInterface('mongodb://localhost:27017/evm-translator')
-    await db.connect()
-    const translator = await getTranslator()
-    await translator.allDataFromTxHashArr(flattenTxHashes())
-    process.exit(0)
+  const translator = await getTranslator()
+  await translator.allDataFromTxHashArr(flattenTxHashes())
+  process.exit(0)
 }
 
 run()
